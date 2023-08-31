63ccbbf0c9de059170de2f4a10c80b5804dcdf0d
dh9999999999999996 <aaw987@naver.com>
2023-08-31 09:58:10 +0900

    프로젝트 초기화

---
+++ html/html실습파일/projectRiotGames/js/common.js
@@ -0,0 +1,13 @@
+$('.footer').load('footer.html')
+//http://ddragon.leagueoflegends.com/cdn/13.17.1/data/ko_KR/champion.json
+//https://developer.riotgames.com/docs/lol#game-client-api_live-client-data-api
+
+/* {
+        "title" : "",
+        "category" : "",
+        "imgLink" : "",
+        "game" : ""
+    } */
+
+
+
